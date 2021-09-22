47caff2875caebac905fb72c99091be368e6f475
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-09-22 13:16:47 +0200

    change ports pririty regarding deploying to heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const winston = require('winston');
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const logger = winston.createLogger({
   level: 'info',
